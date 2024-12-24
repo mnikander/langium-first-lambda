34c7681f1e974d3f0820dd93348427200a2fbb2d
Marco Nikander <m4n1k.developer@gmail.com>
2024-12-24 10:57:25 +0100

    disable a few things which are unused for the moment

--- src/language/first-lambda-validator.ts
+++ src/language/first-lambda-validator.ts
@@ -1,5 +1,7 @@
-import type { ValidationAcceptor, ValidationChecks } from 'langium';
-import type { FirstLambdaAstType, Person } from './generated/ast.js';
+// import type { ValidationAcceptor, ValidationChecks } from 'langium';
+// import type { FirstLambdaAstType, LetStatement } from './generated/ast.js';
+import type { ValidationChecks } from 'langium';
+import type { FirstLambdaAstType } from './generated/ast.js';
 import type { FirstLambdaServices } from './first-lambda-module.js';
 
 /**
@@ -9,7 +11,7 @@ export function registerValidationChecks(services: FirstLambdaServices) {
     const registry = services.validation.ValidationRegistry;
     const validator = services.validation.FirstLambdaValidator;
     const checks: ValidationChecks<FirstLambdaAstType> = {
-        Person: validator.checkPersonStartsWithCapital
+        // Person: validator.checkPersonStartsWithCapital
     };
     registry.register(checks, validator);
 }
@@ -19,13 +21,13 @@ export function registerValidationChecks(services: FirstLambdaServices) {
  */
 export class FirstLambdaValidator {
 
-    checkPersonStartsWithCapital(person: Person, accept: ValidationAcceptor): void {
-        if (person.name) {
-            const firstChar = person.name.substring(0, 1);
-            if (firstChar.toUpperCase() !== firstChar) {
-                accept('warning', 'Person name should start with a capital.', { node: person, property: 'name' });
-            }
-        }
-    }
+    // checkPersonStartsWithCapital(person: Person, accept: ValidationAcceptor): void {
+    //     if (person.name) {
+    //         const firstChar = person.name.substring(0, 1);
+    //         if (firstChar.toUpperCase() !== firstChar) {
+    //             accept('warning', 'Person name should start with a capital.', { node: person, property: 'name' });
+    //         }
+    //     }
+    // }
 
 }
